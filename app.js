3cadeeb06edf4c8d4d7eca10712bdab7a2768273
Shashank <sahushashank21@gmail.com>
2021-03-26 13:23:14 +0530

    v1.0

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
-// app.get("/askAiden", (req, res) => {
-//     res.sendFile(__dirname + "/chatbot/index.html");
-// }); //test
+app.get("/askAiden", (req, res) => {
+    res.render("chatbot");
+});
 
 app.listen(process.env.PORT || port, () => {
     console.log("Server Started at " + port);
