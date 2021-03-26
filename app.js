aa1bef97fb3e9d2289bdcf9efb5febb9d4594856
Shashank <sahushashank21@gmail.com>
2021-03-26 17:49:46 +0530

    Final Commit before submition

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-require('dotenv').config()
 const express = require("express");
 const app = express();
-const port = 3000; //Delete
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -15,4 +13,4 @@ app.get("/askAiden", (req, res) => {
     res.render("chatbot");
 });
 
-app.listen(process.env.PORT || port);
+app.listen(process.env.PORT);
